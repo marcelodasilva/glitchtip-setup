ce56396e3a937c36180f25c0a87228c9096e6012
Marcelo da Silva <marcelo.t.pain@gmail.com>
2023-11-26 21:16:26 +0000

    app example

---
+++ app.py
@@ -0,0 +1,19 @@
+gimport sentry_sdk
+
+import logging
+
+sentry_sdk.init('http://d135c2fef28f41e3972b7b1c5f59cf43@localhost:8000/1')
+
+
+
+# logger = logging.getLogger()
+
+# logger.setLevel(logging.DEBUG)
+
+# logger.debug('DEBUG MESSAGE')
+# # logger.info('INFO MESSAGE')
+# # logger.warning('WARNING MESSAGE')
+# # logger.error('ERROR MESSAGE')
+# # logger.fatal('FATAL MESSAGE')
+
+# # division_by_zero = 1 / 0
